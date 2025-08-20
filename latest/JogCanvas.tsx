1f4eb8cf613fc4f8313ad913c30ae3e89c06aee2
Kevin Staunton-Lambert <music@pyrmontbrewery.com.au>
2025-08-20 11:26:35 +1000

    (chore) MidiKeys React component

--- latest/JogCanvas.tsx
+++ latest/JogCanvas.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useRef, useEffect, useImperativeHandle, forwardRef } from 'react';
 
 interface JogCanvasProps {
     src?: string;
@@ -6,17 +6,43 @@ interface JogCanvasProps {
     width?: number | string;
     height?: number | string;
     style?: React.CSSProperties;
+    midiData?: { note: number; velocity: number; isCC: boolean } | null;
 }
 
-const JogCanvas: React.FC<JogCanvasProps> = ({
+export interface UpdateCanvasDataArgs {
+    pitch: number;
+    velocity: number;
+    cc: boolean;
+}
+export interface JogCanvasHandle {
+    callKasmFunction: (func: string, args?: UpdateCanvasDataArgs) => void;
+}
+const JogCanvas = forwardRef<JogCanvasHandle, JogCanvasProps>(({
     src = '/latest/kasm_canvas_jog_obs.html',
     title = 'Jog Canvas',
     width = 150,
     height = 150,
     style = {},
-}) => {
+    midiData,
+}, ref) => {
+    const iframeRef = useRef<HTMLIFrameElement>(null);
+    useEffect(() => {
+        if (midiData && iframeRef.current && iframeRef.current.contentWindow) {
+            iframeRef.current.contentWindow.postMessage({ type: 'MIDI_DATA', ...midiData }, '*');
+        }
+    }, [midiData]);
+
+    useImperativeHandle(ref, () => ({
+        callKasmFunction: (func: string, args?: UpdateCanvasDataArgs) => {
+            if (iframeRef.current && iframeRef.current.contentWindow) {
+                iframeRef.current.contentWindow.postMessage({ type: 'KASM', func, args }, '*');
+            }
+        }
+    }));
+
     return (
         <iframe
+            ref={iframeRef}
             src={src}
             title={title}
             width={width}
@@ -24,7 +50,7 @@ const JogCanvas: React.FC<JogCanvasProps> = ({
             style={{ border: '1px solid #ccc', borderRadius: '8px', ...style }}
         />
     );
-};
+});
 
 export default JogCanvas;
 
--- src/components/NewsNewFeatures.tsx
+++ src/components/NewsNewFeatures.tsx
@@ -1,13 +1,37 @@
-import React from 'react';
+import React, { useRef, useEffect } from 'react';
 import JogCanvas from '../../latest/JogCanvas';
+import type { JogCanvasHandle } from '../../latest/JogCanvas';
 
 const NewsNewFeatures: React.FC = () => {
-  return (
-      <p>
-          New Component in v1.14!<br/>Jog - video jogger canvas kasm_canvas_jog.rs<br/>
-          <JogCanvas />
-      </p>
-  );
+    const jogCanvasRef = useRef<JogCanvasHandle>(null);
+    useEffect(() => {
+        // Generate a random note in range 40 to 80, intervals of 5
+        function getRandomNote() {
+            const min = 40;
+            const max = 80;
+            const step = 5;
+            const steps = Math.floor((max - min) / step) + 1;
+            return min + step * Math.floor(Math.random() * steps);
+        }
+        const velocity = 100; // Example velocity
+        const interval = setInterval(() => {
+            const note = getRandomNote();
+            const handle = jogCanvasRef.current as JogCanvasHandle | null;
+            if (handle) {
+                handle.callKasmFunction('update_canvas_data', { pitch: note, velocity, cc: false });
+            }
+            if (handle) {
+                handle.callKasmFunction('update_canvas_data', { pitch: note - 10, velocity: 0, cc: false });
+            }
+        }, 500); // every 3 seconds
+        return () => clearInterval(interval);
+    }, []);
+    return (
+        <p>
+            New Component in v1.14!<br/>Jog - video jogger canvas kasm_canvas_jog.rs<br/>
+            <JogCanvas ref={jogCanvasRef} />
+        </p>
+    );
 };
 
 export default NewsNewFeatures;
